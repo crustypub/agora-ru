bd22cb3e086a4b07b7b495e8edbf15daba38ec9a
md <texet0598@gmail.com>
2026-04-04 15:32:19 +0300

    refactor(frontend): init base layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="default-layout">
+    <header>
+      <h1>Мой сайт</h1>
+    </header>
+    
+    <main>
+      <slot /> 
+    </main>
+    
+    <footer>
+      <p>© 2024 Мой сайт</p>
+    </footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.default-layout {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-01-01',
     devtools: { enabled: true },
 
+    css: ['~/assets/css/reset.css'],
+
     devServer: {
         host: '0.0.0.0',
         port: 3000,
