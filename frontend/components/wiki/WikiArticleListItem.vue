ede9f5bd9245f8279efc7c73d111d379fee7f5f2
md <murad.ibragimov@zfera.tech>
2026-05-18 14:51:32 +0300

    feat(frontend): wiki article card status info

--- frontend/components/wiki/WikiArticleListItem.vue
+++ frontend/components/wiki/WikiArticleListItem.vue
@@ -8,18 +8,25 @@
         <div class="article__info">
             <div class="user-wrapper">
                 <span class="user-wrapper__title">Автор: </span>
-                <UTooltip :text="`${data?.created_by?.first_name}  @${data?.created_by?.username}`">
+                <UTooltip :text="`${data?.created_by?.first_name}  @${data?.created_by?.username}`" :delay-duration="0">
                     <UAvatar :src="data.created_by.avatar_url || ''" :alt="data?.created_by?.first_name || '.'"
                         size="xs" />
                 </UTooltip>
             </div>
             <div class="user-wrapper">
                 <span class="user-wrapper__title">Последняя редакция: </span>
-                <UTooltip :text="`${data?.last_edited_by?.first_name}  @${data?.last_edited_by?.username}`">
+                <UTooltip :text="`${data?.last_edited_by?.first_name}  @${data?.last_edited_by?.username}`"
+                    :delay-duration="0">
                     <UAvatar :src="data.last_edited_by.avatar_url || ''" :alt="data?.last_edited_by?.first_name || '.'"
                         size="xs" />
                 </UTooltip>
             </div>
+            <UTooltip text="Статья подтверждена" :delay-duration="0" class="article-status" v-if="!!data.is_confirmed">
+                <UIcon name="material-symbols:check-circle-rounded" class="text-success-500 size-5" />
+            </UTooltip>
+            <UTooltip text="Статья не подтверждена" :delay-duration="0" class="article-status" v-else-if="!data.is_confirmed">
+                <UIcon name="material-symbols:info" class="text-error-500 size-5" />
+            </UTooltip>
         </div>
     </NuxtLink>
 </template>
@@ -52,7 +59,6 @@ const { data } = defineProps<IProps>();
     box-shadow: 0 2px 4px rgba($black, 0.02);
 
     &:hover {
-        transform: translateY(-2px);
         box-shadow: 0 6px 12px rgba($black, 0.08);
         border-color: $primary;
 
@@ -83,6 +89,10 @@ const { data } = defineProps<IProps>();
         gap: 1.25rem;
         padding-top: 0.75rem;
         border-top: 1px solid $gray-200;
+
+        .article-status {
+            margin-left: auto;
+        }
 
         .user-wrapper {
             display: flex;
